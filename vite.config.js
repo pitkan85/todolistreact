da73eb4e77c331523d6054e7b069a2ee1139e88d
pitkan85 <152480828+pitkan85@users.noreply.github.com>
2023-12-03 17:52:09 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/todolistreact/', // add this property and the value is the name of your repository
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  } 
 })
